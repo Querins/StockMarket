3e7503313362da17216058b1be921d380307a6d9
Alex Querins <querins777@gmail.com>
2017-07-03 11:43:53 +0300

    mend

---
+++ src/main/java/com/querins/StockMarket/StockMarketApplication.java
@@ -0,0 +1,12 @@
+package com.querins.StockMarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockMarketApplication.class, args);
+	}
+}
---
+++ src/test/java/com/querins/StockMarket/StockMarketApplicationTests.java
@@ -0,0 +1,16 @@
+package com.querins.StockMarket;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StockMarketApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
