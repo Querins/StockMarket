4c1e63031603e3ef43e1fc77928fc94d01c1fd1a
Alex Querins <querins777@gmail.com>
2017-07-21 13:51:00 +0300

    Home page controller added.

--- src/main/java/com/querins/StockMarket/controllers/MainController.java
+++ src/main/java/com/querins/StockMarket/controllers/MainController.java
@@ -1,7 +1,17 @@
 package com.querins.StockMarket.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
 /**
  * Created by roman on 03.07.17.
  */
+@Controller
 public class MainController {
+
+    @GetMapping({"/", "/home"})
+    String home() {
+        return "index";
+    }
+
 }
